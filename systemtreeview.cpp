99f125939bc154315c7bf984bd1b28f34e4a23ba
afigegoznaet <enrabiar@gmail.com>
2018-05-18 22:04:14 +0300

    Fixed root index

--- systemtreeview.cpp
+++ systemtreeview.cpp
@@ -3,97 +3,97 @@
 
 
 SystemTreeView::SystemTreeView(QWidget *parent) : QTreeView(parent){
-    fsModel = new ThumbnailsFileModel(this);
-    auto model = new QFileSystemModel(this);
+	fsModel = new ThumbnailsFileModel(this);
+	auto model = new QFileSystemModel(this);
 
-    auto parentWindow = qobject_cast<MainWindow*>(parent);
-    auto parentObject = parent->parent();
-    while(nullptr == parentWindow){
-        parentWindow = qobject_cast<MainWindow*>(parentObject);
-        parentObject = parentObject->parent();
-    }
-    auto rootPath = parentWindow->getRoot();
+	auto parentWindow = qobject_cast<MainWindow*>(parent);
+	auto parentObject = parent->parent();
+	while(nullptr == parentWindow){
+		parentWindow = qobject_cast<MainWindow*>(parentObject);
+		parentObject = parentObject->parent();
+	}
+	auto rootPath = parentWindow->getRoot();
 
-    connect(fsModel, SIGNAL(splashText(QString,int,QColor)),
-            this, SIGNAL(splashText(QString,int,QColor)));
+	connect(fsModel, SIGNAL(splashText(QString,int,QColor)),
+			this, SIGNAL(splashText(QString,int,QColor)));
 
-    auto runner = QtConcurrent::run([&, rootPath](){
-        fsModel->scanRoot(rootPath);
-    });
-
-
-    qDebug()<<"Root: "<<rootPath;
-    model->setRootPath(rootPath);
-    model->setFilter(QDir::AllDirs | QDir::NoDotAndDotDot);
-
-    for (int i = 1; i < model->columnCount(); ++i)
-        hideColumn(i);
-
-    fsModel->setSourceModel(model);
-    fsModel->setDynamicSortFilter(false);
-    setModel(fsModel);
-    setRootIndex(fsModel->index(0,0));
+	auto runner = QtConcurrent::run([&, rootPath](){
+		fsModel->scanRoot(rootPath);
+	});
 
 
-    for (int i = 1; i < model->columnCount(); ++i)
-        hideColumn(i);
+	qDebug()<<"Root: "<<rootPath;
+	auto rootIndex = model->setRootPath(rootPath);
+	model->setFilter(QDir::AllDirs | QDir::NoDotAndDotDot);
 
-    connect(selectionModel(), &QItemSelectionModel::currentChanged,
-            [&](QModelIndex current, QModelIndex){
-                emit changeDir(fsModel->fileInfo(current).absoluteFilePath());
-            });
+	for (int i = 1; i < model->columnCount(); ++i)
+		hideColumn(i);
+
+	fsModel->setSourceModel(model);
+	fsModel->setDynamicSortFilter(false);
+	setModel(fsModel);
+	setRootIndex(fsModel->mapFromSource(rootIndex));
+
+
+	for (int i = 1; i < model->columnCount(); ++i)
+		hideColumn(i);
+
+	connect(selectionModel(), &QItemSelectionModel::currentChanged,
+			[&](QModelIndex current, QModelIndex){
+				emit changeDir(fsModel->fileInfo(current).absoluteFilePath());
+			});
 
 }
 
 void SystemTreeView::init(QString& startDir){
 
-    qDebug()<<"startDir: "<<startDir;
-    runner.waitForFinished();
+	qDebug()<<"startDir: "<<startDir;
+	runner.waitForFinished();
 
 
 
-    auto idx = fsModel->fileIndex(startDir);
-    //qDebug()<<"idx: "<<idx.isValid();
-    //setCurrentIndex(idx);
-    expand(idx);
-    scrollTo(idx);
+	auto idx = fsModel->fileIndex(startDir);
+	//qDebug()<<"idx: "<<idx.isValid();
+	//setCurrentIndex(idx);
+	expand(idx);
+	scrollTo(idx);
 
-    QDir dir(startDir);
+	QDir dir(startDir);
 
-    while(dir.cdUp()){
+	while(dir.cdUp()){
 
-        runner = fsModel->scanTreeAsync(dir.absolutePath());
-        runner.waitForFinished();
-        auto idx = fsModel->fileIndex(dir.absolutePath());
-        //setCurrentIndex(idx);
-        //qDebug()<<"idx: "<<idx.isValid();
-        expand(idx);
-        //setCurrentIndex(idx);
-        scrollTo(idx);
+		runner = fsModel->scanTreeAsync(dir.absolutePath());
+		runner.waitForFinished();
+		auto idx = fsModel->fileIndex(dir.absolutePath());
+		//setCurrentIndex(idx);
+		//qDebug()<<"idx: "<<idx.isValid();
+		expand(idx);
+		//setCurrentIndex(idx);
+		scrollTo(idx);
 
-    }
+	}
 
 
-    idx = fsModel->fileIndex(startDir);
-    //qDebug()<<"idx: "<<idx.isValid();
+	idx = fsModel->fileIndex(startDir);
+	//qDebug()<<"idx: "<<idx.isValid();
 
-    expand(idx);
-    scrollTo(idx);
-    setCurrentIndex(idx);
+	expand(idx);
+	scrollTo(idx);
+	setCurrentIndex(idx);
 
 }
 
 QString SystemTreeView::getCurrentDir(){
-    auto idx = currentIndex();
-    qDebug()<<"currentDir";
-    return fsModel->fileInfo(idx).absoluteFilePath();
+	auto idx = currentIndex();
+	qDebug()<<"currentDir";
+	return fsModel->fileInfo(idx).absoluteFilePath();
 }
 
 void SystemTreeView::initDir(QString& startDir){
-    auto idx = fsModel->fileIndex(startDir);
-    //qDebug()<<"idx: "<<idx.isValid();
-    setCurrentIndex(idx);
-    expand(idx);
-    scrollTo(idx);
+	auto idx = fsModel->fileIndex(startDir);
+	//qDebug()<<"idx: "<<idx.isValid();
+	setCurrentIndex(idx);
+	expand(idx);
+	scrollTo(idx);
 
 }
